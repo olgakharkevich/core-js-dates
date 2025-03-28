bcb550e8025d3db3e887a19765d46da5c1c2325b
Olga Kharkevich <olga.kharkevich@gmail.com>
2025-03-28 09:59:54 -0400

    feat: add solution for getNextFriday

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -75,8 +75,11 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const difMs = ((5 - date.getUTCDay() + 7) % 7 || 7) * 24 * 60 * 60 * 1000;
+  const nextFriday = new Date();
+  nextFriday.setTime(date.getTime() + difMs);
+  return nextFriday;
 }
 
 /**
